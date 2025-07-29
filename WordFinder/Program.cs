8bcc780b3a0aa7431cae880ee707acb385f75bcb
Eze Morales <ezhemorales@gmail.com>
2025-07-29 02:34:47 -0300

    Added method to print words to be searched and method calls in main method to print the matrix and the words

--- WordFinder/Program.cs
+++ WordFinder/Program.cs
@@ -1,6 +1,9 @@
 ﻿using WordSearchApp.Core;
 using WordSearchApp.Utils;
 
+WordFinderPrinter.PrintMatrix(TestData.Matrix);
+WordFinderPrinter.PrintWordsToSearch(TestData.WordsToSearch);
+
 var wordFinder = new WordFinder(TestData.Matrix);
 
 var foundWords = wordFinder.Find(TestData.WordsToSearch);
--- WordFinder/Utils/WordFinderPrinter.cs
+++ WordFinder/Utils/WordFinderPrinter.cs
@@ -15,7 +15,7 @@ namespace WordSearchApp.Utils
 
             for (int i = 0; i < matrixList.Count; i++)
             {
-                Console.WriteLine($"Row {i}: {string.Join(" ", matrixList[i].ToCharArray())}");
+                Console.WriteLine($"{string.Join(" ", matrixList[i].ToCharArray())}");
             }
             Console.WriteLine(new string('-', 30));
         }
@@ -34,6 +34,27 @@ namespace WordSearchApp.Utils
 
             Console.WriteLine($"Total words found: {wordsList.Count}");
             Console.WriteLine("Words (ordered by frequency):");
+
+            for (int i = 0; i < wordsList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1,2}. {wordsList[i]}");
+            }
+
+            Console.WriteLine(new string('-', 30));
+        }
+
+        public static void PrintWordsToSearch(IEnumerable<string> words, string title = "Words to Search")
+        {
+            Console.WriteLine($"\n=== {title} ===");
+            var wordsList = words.ToList();
+
+            if (!wordsList.Any())
+            {
+                Console.WriteLine("No words to search.");
+                return;
+            }
+
+            Console.WriteLine($"Total words to search: {wordsList.Count}");
 
             for (int i = 0; i < wordsList.Count; i++)
             {
